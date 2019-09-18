9d551263e1a5c5c47f6e2a3f8c53335f0cde43c0
Nicolas <nicolico99@hotmail.com>
2019-09-18 18:48:58 +0000

    9 try 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10");
 	return (1);
 }
