96eda800bbf0fe38ab2c12513f431acd5a62004b
Nicolas <nicolico99@hotmail.com>
2019-09-19 23:46:51 +0000

    102 try 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,6 +25,7 @@ int main(void)
 		{
 			putchar(a / 10 + '0');
 			putchar(a % 10 + '0');
+			putchar(' ');
 			putchar(b / 10 + '0');
 			putchar(b % 10 + '0');
 			if (!(a == 98 && b == 99))
@@ -32,7 +33,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
-}
+		}
 		b = p[c];
 		c++;
 	}
