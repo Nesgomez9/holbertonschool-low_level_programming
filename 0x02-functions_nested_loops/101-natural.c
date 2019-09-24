58a4c5ac35f8222aaa4d249cf362b6072c34ca32
nicolas <1252@holbertonschool.com>
2019-09-24 23:02:44 +0000

    13,14

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main (void)
+{
+	int a, n;
+	n = 0;
+
+	for(a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			n = n + a;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main (void)
+{
+	unsigned long int a, b, i;
+
+	a = 1;
+	b = 2;
+
+	for(i = 0; i <= 50; i++)
+	{
+		printf("%lu, ", a);
+		printf("%lu, ", b);
+		a = a + b;
+		b = b + a;
+	}
+	printf("\n");
+	return (0);
+}
