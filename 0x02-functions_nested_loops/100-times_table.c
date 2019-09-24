8ed0b169e84783a13583c8c86eb2b83a691af7a9
nicolas <1252@holbertonschool.com>
2019-09-24 22:18:14 +0000

    12 fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
-#include "holberton.h"
+#include "holberton."
 
- /**
+/**
  * print_times_table - writes the character c to stdout
- *
  * @n: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,6 +12,7 @@
 void print_times_table(int n)
 {
 	int a, b;
+
 	if (n <= 15 && n >= 0)
 	{
 		for (a = 0; a <= n; a++)
@@ -20,35 +21,33 @@ void print_times_table(int n)
 			{
 				if (b == 0)
 				{
-					_putchar((a*b) + '0');
-				}
-				else if (((a * b) / 10) < 1)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((a * b) + '0');
-				}
-				else if ((((a * b) / 10) >= 1) && (((a * b) / 10) < 10))
-				{
-					_putchar(',');
-                                        _putchar(' ');
-                                        _putchar(' ');
-					_putchar(((a * b) / 10) + '0');
-					_putchar(((a * b) % 10) + '0');
 				}
 				else
 				{
 					_putchar(',');
-                                        _putchar(' ');
-					_putchar(((a * b) / 100) + '0');
-					_putchar((((a * b) % 100) / 10 )+ '0');
-					_putchar((((a * b) % 100) % 10 )+ '0');
-
+					_putchar(' ');
+					if (((a * b) / 10) < 1)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((a * b) + '0');
+					}
+					else if ((((a * b) / 10) >= 1) && (((a * b) / 10) < 10))
+					{
+						_putchar(' ');
+						_putchar(((a * b) / 10) + '0');
+						_putchar(((a * b) % 10) + '0');
+					}
+					else
+					{
+						_putchar(((a * b) / 100) + '0');
+						_putchar((((a * b) % 100) / 10) + '0');
+						_putchar((((a * b) % 100) % 10) + '0');
+					}
 				}
 			}
 			_putchar('\n');
+			}
 		}
-	}
 }
