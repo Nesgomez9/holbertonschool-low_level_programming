4c708db22dcacac66786f82475ae41d074bc143d
nicolas <1252@holbertonschool.com>
2019-09-26 18:56:36 +0000

    11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	int i;
+	long long int a;
+
+	a = 612852475143;
+
+	for  (i = 2; i <= 10000; i++)
+	{
+		if (a % i == 0 )
+		{
+			if ((i == 2) || (i == 3) || (i == 5) || (i == 7))
+			{
+				a = a / i;
+			}
+			else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
+			{
+				a = a / i;
+			}
+		}
+
+	}
+	printf("%lld", a);
+	printf("\n");
+	return (0);
+}
