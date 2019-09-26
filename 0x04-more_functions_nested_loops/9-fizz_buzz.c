e33a19a470e334cea10ce8bd3f0228ce0f94e6da
nicolas <1252@holbertonschool.com>
2019-09-26 18:07:12 +0000

    9 fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,14 +23,19 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
+		else if (i % 3 != 0 && i % 5 == 0 && i != 100)
+		{
+			printf("Buzz");
+		}
 		else if (i % 3 != 0 && i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
+
 	}
 	printf("\n");
 	return (0);
