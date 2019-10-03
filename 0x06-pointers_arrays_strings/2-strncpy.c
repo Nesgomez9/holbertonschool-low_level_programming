98c57677149510f37ba1142b3721f989efd3ea4e
Nicolas <nicolico99hotmail.com>
2019-10-03 20:04:21 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Function that concatenates two strings.
+ * @dest: String destination.
+ * @src: Source String.
+ * @n: int.
+ *
+ * Return: dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	while (b < n)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+	return (dest);
+}
