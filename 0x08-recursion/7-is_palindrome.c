02bef94e88047eea6f4df52be7be33161e6794ec
Nicolas <nicolico99hotmail.com>
2019-10-10 03:07:51 +0000

    7 try 1

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,21 +1,47 @@
 #include "holberton.h"
 
 /**
+ * _strlen_recursion - print the length of a string
+ * @s: array to count the length
+ *
+ * Return: the length
+ */
+
+int _strlen_recursion(char *s)
+{
+
+        if (*s != 0)
+        {
+                return (1 + _strlen_recursion(s + 1));
+        }
+        else
+        {
+                return (0);
+        }
+}
+
+/**
  * is_palindrome1 - Prints the array reverse
  * @s: array to rev
  *
  * Return: Nothing
  */
-int is_palindrome1(char *s, int len)
+int is_palindrome1(char *s, int i, int len)
 {
-	if (*s != *(s + len))
-		return (0);
-	else if (*s == 0)
+	if (i >= len)
+	{
 		return (1);
-	return(is_palindrome1(s + 1, len - 1));
+	}
+	if (s[len] == s[i])
+	{
+		return (is_palindrome1(s, len - 1, i + 1));
+	}
+	return (0);
 }
 
 int is_palindrome(char *s)
 {
-	return (is_palindrome1(s, 5));
+	int i = 0;
+	int len = _strlen_recursion(s);
+	return (is_palindrome1(s, i, len - 1));
 }
