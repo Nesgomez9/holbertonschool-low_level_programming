b3bbd02d918754be45e9a5edc246aed43f0a7b93
Nicolas <nicolico99hotmail.com>
2019-10-15 18:20:08 +0000

    0 -fix 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,10 @@ char *create_array(unsigned int size, char c)
 		return (dest);
 	}
 	dest = malloc(size * sizeof(char));
+	if (dest == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < size; i++)
 		dest[i] = c;
 	dest[i] = '\0';
