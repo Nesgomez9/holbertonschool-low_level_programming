8048922222bf35fbf4647f9a611ae58bbd914a93
Nicolas Gomez <1252@holbertonschool.com>
2019-10-15 19:47:11 +0000

    2 fix 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,17 +22,16 @@ char *str_concat(char *s1, char *s2)
 	{}
 	for (size2 = 0; s2[size2]; size2++)
 	{}
+
 	dest = malloc((size1 + size2 + 1) * sizeof(char));
+
 	if (dest == 0)
-	{
 		return (NULL);
-	}
-	for (i = 0, j = 0; i < (size1 + size2 + 1); i++)
-	{
-		if (i < size1)
-			dest[i] = s1[i];
-		else
-			dest[i] = s2[j++];
-}
+
+	for (i = 0; i < size1 ; i++)
+		dest[i] = s1[i];
+	for (i = 0, j = size1; i <= size2; i++, j++)
+		dest[j] =s2[i];
+
 	return (dest);
 }
