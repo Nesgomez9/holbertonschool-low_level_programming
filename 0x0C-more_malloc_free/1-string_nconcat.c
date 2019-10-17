d84e74ab5977cb8189357c698f651d60fff2afa7
Nicolas Gomez <1252@holbertonschool.com>
2019-10-17 17:42:21 +0000

    Task 1 Fix 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,32 +12,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, cont, a;
+	unsigned int i, cont, a;
 	char *dest;
 
 	if (!s1)
-		s1 = "";
+		s1 = 0;
 	else
 		for (i = 0; s1[i]; i++)
 		{}
 
 	if (!s2)
-		s2 = "";
-	else
-		for (j = 0; s2[j]; j++)
-		{}
-	if (n > j)
-		n = j;
+		n = 0;
 
 	dest = malloc((n + i + 1) * sizeof(char));
 	if (!dest)
 		return (NULL);
-	for (cont = 0, a = 0; cont < (n + i + 1); cont++)
+	for (cont = 0, a = 0; cont < (n + i); cont++)
 	{
 		if (cont < i)
 			dest[cont] = s1[cont];
 		else
 			dest[cont] = s2[a++];
 	}
+	dest[cont] = 0;
 	return (dest);
 }
