2529801728d9d82252ae9f6be736bc714a0df839
Nicolas Gomez <1252@holbertonschool.com>
2019-10-21 17:45:11 +0000

    Task 2 Fix 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,20 +12,17 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if((*d).name)
+		if ((*d).name)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)\n");
 
-		if((*d).age)
-                        printf("Age: %.6f\n", (*d).age);
-                else
-                        printf("Age: (nil)\n");
+		printf("Age: %.6f\n", (*d).age);
 
-		if((*d).owner)
-                        printf("Owner: %s\n", (*d).owner);
-                else
-                        printf("Owner: (nil)\n");
+		if ((*d).owner)
+			printf("Owner: %s\n", (*d).owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 	else
 		return;
