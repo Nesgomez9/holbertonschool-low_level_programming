ffeca574dffb9b36adb7aed186a562c53bbd2085
Nicolas Gomez <1252@holbertonschool.com>
2019-10-21 17:47:11 +0000

    Task 1 fix 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * inti_dog - check the code for Holberton School students.
+ * init_dog - check the code for Holberton School students.
  * @d: the struct
  * @name: name of the dog
  * @age: age of the dog
@@ -12,8 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d)
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
-	return;
+	else
+		return;
 }
