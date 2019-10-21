782932ac2a1b810167b0d50eaf3e0a1d71f15be9
Nicolas Gomez <1252@holbertonschool.com>
2019-10-21 17:37:29 +0000

    Task 2 Fix 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,4 +16,6 @@ void print_dog(struct dog *d)
 		((*d).age ? printf("Age: %.6f\n", (*d).age): printf("Age: (nil)\n"));
 		((*d).owner ? printf("Owner: %s\n", (*d).owner) : printf("Owner: (nil)\n"));
 	}
+	else
+		return;
 }
