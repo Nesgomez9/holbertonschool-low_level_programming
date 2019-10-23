df0513d2194edffb4d13641a5f99371d408e3f94
Nicolas Gomez <1252@holbertonschool.com>
2019-10-23 16:55:49 +0000

    Task 0,1 and 2 Fix 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,9 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: function
+ * int_index - prints a name as is
+ * @array: name of the person
+ * @size: function
+ * @cmp: function
  *
  * Return: Nothing.
  */
@@ -14,9 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for(i = 0; i < size; i++)
+	if (!array || !cmp)
+		return(NULL);
+	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]))
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
