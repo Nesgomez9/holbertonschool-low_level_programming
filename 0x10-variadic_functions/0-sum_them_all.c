a9334c2e3a470b0ba7b074701ffa12a056d78f6b
Nicolas Gomez <1252@holbertonschool.com>
2019-10-27 22:39:11 +0000

    Task 0 Fix 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,8 +5,10 @@
 /**
  * sum_them_all - check the code for Holberton School students.
  * @n: Number
+ * @...: Arguments Variadic
  * Return: Always 0.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list numb;
