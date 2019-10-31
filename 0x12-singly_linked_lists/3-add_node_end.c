e4f4128a23780a4cdca7487cf9c4dba9a08ac353
nicolas9909 <nicolico99@hotmail.com>
2019-10-31 19:57:46 +0000

    Fix task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,32 +1,5 @@
 #include "lists.h"
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory, which
- * contains a copy of the string given as a parameter.
- * @str: string to copy
- *
- * Return: Pointer
- */
-char *_strdup(const char *str)
-{
-	int l = 0, i;
-	char *s;
-
-	if (str == NULL)
-		return (0);
-
-	while (*(str + l))
-		l++;
-
-	s = malloc(sizeof(char) * l + 1);
-
-	if (s == 0)
-		return (0);
-
-	for (i = 0; i <= l; i++)
-		*(s + i) = *(str + i);
-	return (s);
-}
+#include <string.h>
 
 /**
  * _strlen - writes the character c to stdout
@@ -35,7 +8,7 @@ char *_strdup(const char *str)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int i = 0;
 
@@ -55,7 +28,6 @@ int _strlen(char *s)
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *s;
 	list_t *temp = *head;
 
 	new_node = malloc(sizeof(list_t));
@@ -70,12 +42,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		s = _strdup(str);
-		if (!s)
-			return (NULL);
-
-		new_node->str = s;
-		new_node->len = _strlen(s);
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
 	}
 	new_node->next = NULL;
 	if (!temp)
