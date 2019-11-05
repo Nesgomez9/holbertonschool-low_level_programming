105ea8e02997bae6520e5b15daec5ffc98ce137e
nicolas9909 <nicolico99@hotmail.com>
2019-11-05 16:00:30 +0000

    Task 10 fix 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *temp2;
 
-	if (!head && !*head)
+	if (!head || !*head)
 		return (-1);
 	if (!index && !*head)
 	{
