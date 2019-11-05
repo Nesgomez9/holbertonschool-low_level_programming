e69c2550d8709378552f5fa8b677ffd3419e9dd4
nicolas9909 <nicolico99@hotmail.com>
2019-11-05 16:08:50 +0000

    Task 9 try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!n_node)
 		return (NULL);
-	if (!head && !*head)
+	if (!head)
 		return (NULL);
 	n_node->n = n;
 	if (!idx)
