1448aced536589303d787237527c51b943f67d31
nicolas9909 <nicolico99@hotmail.com>
2019-11-07 19:54:21 +0000

    Task 4 fix 3

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,14 +11,15 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i;
 	unsigned long int bin = 1;
+	unsigned long int a = *n;
 
 	if (index > 63)
 		return (-1);
 
 	for (i = 0; i < index; i++)
 		bin *= 2;
-
-	if (!(*n ^ bin))
+	a = ((a >> index) & 1);
+	if (a)
 		*n ^= bin;
 
 	return (1);
