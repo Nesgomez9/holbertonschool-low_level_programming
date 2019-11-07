ca9c33139ad3bdf39e4a4959797d2eeb31dd0433
nicolas9909 <nicolico99@hotmail.com>
2019-11-07 18:11:11 +0000

    Task 1

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * clear_bit - check the code for Holberton School students.
+ * @n: unsigned int to print
+ * @index: index
+ *
+ * Return: unsigned int.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int bin = 1;
+
+	if (index > 63)
+		return (-1);
+	for (i = 0; i < index; i++)
+		bin *= 2;
+	if (!(*n ^ bin) && (*n & bin))
+		*n ^= bin;
+
+	return (1);
+}
