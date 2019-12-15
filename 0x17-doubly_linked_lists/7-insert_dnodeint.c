d2ace31d1415a0ace00fb2eae1da92612eb5e4f6
Nesgomez9 <nicolico99@hotmail.com>
2019-12-15 19:36:10 +0000

    Task 7 fix 5

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	while (tmp)
 	{
-		if (i == idx -1)
+		if (i == idx - 1)
 		{
 			new->next = tmp->next;
 			new->prev = tmp;
@@ -43,5 +43,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 		tmp = tmp->next;
 	}
+	free(new);
 	return (NULL);
 }
