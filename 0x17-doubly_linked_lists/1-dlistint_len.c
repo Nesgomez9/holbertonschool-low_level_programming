b4fa0bd8ffe49d72217903ad663310d8394ad98c
Nesgomez9 <nicolico99@hotmail.com>
2019-12-15 18:48:47 +0000

    Fixing betty Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,13 +6,13 @@
  *
  * Return: Number of nodes
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
 	while (h)
 	{
-	        h = (*h).next;
+		h = (*h).next;
 		nodes++;
 	}
 	return (nodes);
