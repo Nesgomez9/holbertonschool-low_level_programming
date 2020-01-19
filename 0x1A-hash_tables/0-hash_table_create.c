a3d0dcda10b3e63baaec26e6bda695fd4772368a
Nesgomez9 <nicolico99@hotmail.com>
2020-01-19 21:25:45 +0000

    Adding fist mandatory task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to the new table or NULL in case of fail
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
+
+	return (table);
+}
