e7257deeb6f1830f69a911e34a766974dab18135
Nesgomez9 <nicolico99@hotmail.com>
2020-01-19 23:04:12 +0000

    Fixing task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	collnode->key = keydup;
 	collnode->value = valuedup;
-	collnode->next = hnode;
+	collnode->next = ht->array[index];
 	ht->array[index] = collnode;
 
 	return (1);
