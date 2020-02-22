21b51748259ccb72bcbc2f0bdfcc738b0fefbd9d
Nesgomez9 <nicolico99@hotmail.com>
2020-02-22 03:30:38 +0000

    Adding the mandatory Task and the advanced task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Module of island_perimeter"""
+
+
+def island_perimeter(grid):
+    """ This function find the perimeter of
+    an island"""
+
+    count = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            mul = 4
+            if grid[i][j] == 1:
+                if grid[i][j + 1] == 1:
+                    mul -=1
+                if grid[i][j - 1] == 1:
+                     mul -=1
+                if grid[i + 1][j] == 1:
+                     mul -=1
+                if grid[i - 1][j] == 1:
+                     mul -=1
+            else:
+                continue
+            count += mul
+    return count
