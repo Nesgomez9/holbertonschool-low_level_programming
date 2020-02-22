60f71d33a1e97c80e29017775ddf5515636ee99e
Nesgomez9 <nicolico99@hotmail.com>
2020-02-22 03:53:14 +0000

    fixing island task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,14 +13,16 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             mul = 4
             if grid[i][j] == 1:
-                if grid[i][j + 1] == 1:
+                if j < len (grid[0]) - 1:
+                    if grid[i][j + 1] == 1:
+                        mul -=1
+                if grid[i][j - 1] == 1 and j > 0:
                     mul -=1
-                if grid[i][j - 1] == 1:
-                     mul -=1
-                if grid[i + 1][j] == 1:
-                     mul -=1
-                if grid[i - 1][j] == 1:
-                     mul -=1
+                if i < len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        mul -=1
+                if grid[i - 1][j] == 1 and i > 0:
+                    mul -=1
             else:
                 continue
             count += mul
